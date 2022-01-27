7b098fbc8a21b0c7a95c937e52d3291c42e8c205
annmari07 <51410738+annmari07@users.noreply.github.com>
2022-01-28 00:57:19 +0100

    potion

---
+++ Programowanie/Assets/Scripts/PickupController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+  
+  
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger enter " + other.gameObject.name);
+
+
+        PickupObject pickup = other.gameObject.GetComponent<PickupObject>();
+
+        if(pickup != null)
+            
+            {
+            pickup.OnPickupFound();
+
+            }
+
+    }
+
+
+
+
+}
