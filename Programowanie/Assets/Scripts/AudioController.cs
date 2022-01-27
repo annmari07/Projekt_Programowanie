aa5726b2ce53aaf33d9ed4576a527989ff2dfba5
annmari07 <51410738+annmari07@users.noreply.github.com>
2022-01-27 06:06:09 +0100

    Sounds01

---
+++ Programowanie/Assets/Scripts/AudioController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    
+
+
+
+    public void Step()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
